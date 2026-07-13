d45baa5e9ce35956d813ac17cb72f24192a647cd
kaeruri <likaellechua@gmail.com>
2026-07-13 16:03:11 +0800

    CA3 Task 3: Goal

--- Assets/scripts/GoalArea.cs
+++ Assets/scripts/GoalArea.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 
-public class GoalArea
+public class GoalArea : MonoBehaviour
 {
-    
+    [Header("Score Tracker")]
+    private int score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BallController>() != null)
+        {
+            score++;
+            Debug.Log("GOAL! Total Score: " + score);
+            
+            Destroy(other.gameObject);
+        }
+    }
 }
